f820e5c320fbcc9c469543f4eae8e13cd4c740b8
Ayush <ayushsyntax@gmail.com>
2024-11-01 23:24:02 +0530

    Daily update: describe what you did

--- Step 5 LinkedList Basic/LL04.cpp
+++ Step 5 LinkedList Basic/LL04.cpp
@@ -84,6 +84,7 @@ int main() {
     head = deleteK(head, k); // Delete the k-th node
     cout << "Linked List after deleting " << k << "-th node: "; // Output message after deletion
     traverse(); // Traverse and display the modified linked list
+    
     // Clean up remaining nodes
     while (head != nullptr) {
         Node* temp = head;
